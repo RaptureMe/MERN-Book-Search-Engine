fb89ee337e0e7232beb001d0724672ebe7c7a176
RaptureMe <arosu4u@gmail.com>
2023-11-22 19:36:39 -0800

    added BookInput

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,17 +28,22 @@ const typeDefs = `
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
     me: User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
+    saveBook(book: BookInput): User
     addComment(thoughtId: ID!, commentText: String!): Thought
     removeThought(thoughtId: ID!): Thought
     removeComment(thoughtId: ID!, commentId: ID!): Thought
