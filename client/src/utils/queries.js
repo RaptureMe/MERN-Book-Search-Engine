40d10fabc9bc06ec7ec1aa243a326b33334bcf74
RaptureMe <arosu4u@gmail.com>
2023-11-26 20:06:54 -0800

    finished queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client"
+export const QUERY_ME = gql`
+query Me {
+    me {
+      username
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`
